3bcdd32d7cf7683f4eee2390f08db600ba22ddc8
nmin <nmin1124@gmail.com>
2021-02-10 17:08:20 +0900

    3챕터 게시글 업데이트 테스트 통과

---
+++ src/main/java/com/loko/fascinating/springboot/web/PostsApiController.java
@@ -0,0 +1,31 @@
+package com.loko.fascinating.springboot.web;
+
+import com.loko.fascinating.springboot.service.posts.PostsService;
+import com.loko.fascinating.springboot.web.dto.PostsResponseDto;
+import com.loko.fascinating.springboot.web.dto.PostsSaveRequestDto;
+import com.loko.fascinating.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+        return postsService.save(requestDto);
+    }
+
+    @PostMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById (@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+}
