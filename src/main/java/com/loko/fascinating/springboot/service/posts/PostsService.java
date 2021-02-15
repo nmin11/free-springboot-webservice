c225e7f7c73b497f24b95b4a0876f1c3887b59dc
nmin <nmin1124@gmail.com>
2021-02-15 20:52:04 +0900

    챕터 4 완료

--- src/main/java/com/loko/fascinating/springboot/service/posts/PostsService.java
+++ src/main/java/com/loko/fascinating/springboot/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.loko.fascinating.springboot.service.posts;
 
 import com.loko.fascinating.springboot.domain.posts.Posts;
 import com.loko.fascinating.springboot.domain.posts.PostsRepository;
+import com.loko.fascinating.springboot.web.dto.PostsListResponseDto;
 import com.loko.fascinating.springboot.web.dto.PostsResponseDto;
 import com.loko.fascinating.springboot.web.dto.PostsSaveRequestDto;
 import com.loko.fascinating.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -36,4 +39,20 @@ public class PostsService {
 
         return new PostsResponseDto(entity);
     }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
 }
